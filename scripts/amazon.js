ba48a2374d76c1d221d6300f204f855c7ed36872
NadimElMasry <nadimmasry@ymail.com>
2025-06-14 15:01:54 +0300

    Load imported products array from products.js to replace default data structure on amazon.js; fix image interpolation in HTML string

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,20 +1,4 @@
-const products = [{
-  image: 'athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-}, {
-  image: 'intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-}];
+import {products} from '../data/products.js';
 
 let productsGridHTML = '';
 products.forEach((product) => {
@@ -22,7 +6,7 @@ products.forEach((product) => {
     <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
-          src="images/products/${product.image}">
+          src="${product.image}">
       </div>
 
       <div class="product-name limit-text-to-2-lines">
