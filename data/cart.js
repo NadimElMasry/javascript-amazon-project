aad25c85bc5b34775467b976ef2240250c7312f9
NadimElMasry <nadimmasry@ymail.com>
2025-06-16 12:37:39 +0300

    Try 2 different methods to update cart item quantity

--- data/cart.js
+++ data/cart.js
@@ -4,12 +4,39 @@ export function addToCart() {
   document.querySelectorAll('.js-add-to-cart-button').forEach((addButton) => {
     addButton.addEventListener('click', () => {
       const productId = addButton.dataset.productId;
-      cart.push({
-        id: productId,
-        quantity: 1
-      });      
+
+      const matchingItem = cart.find((cartItem) => {
+        return cartItem.id === productId;
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          id: productId,
+          quantity: 1
+        });
+      }
+
+      /*
+      let matchingItem;
+      cart.forEach((cartItem) => {
+        if (cartItem.id === productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity++;
+      } else {
+        cart.push({
+          id: productId,
+          quantity: 1
+        });
+      }
+      */
+
       localStorage.setItem('cart', JSON.stringify(cart));
-      console.log(cart);
     });
   })
 }
